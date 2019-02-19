e5d316f5915813e7a3cff4b887040bd31dd43adf
sanidhya70 <ksanidhya007@gmail.com>
2019-02-18 21:55:46 -0500

    Created home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        
+      
+        <section style={{
+           backgroundColor: '#B33951',
+           paddingTop : 8,
+           paddingBottom :8
+            
+          }}>
+              <img width="150" src="employee.svg" alt="description of "/>   
+        </section>
+
+        <section style={{
+           backgroundColor: '#2274A5',
+           paddingTop : 8,
+           paddingBottom :8
+            
+          }}>
+        <img width="150" src="portfolio.svg" alt="description dfffof "/>
+        </section>
+
+        <section style={{
+           backgroundColor: '#E9F1F7',
+           paddingTop : 8,
+           paddingBottom :8
+            
+          }}>
+        <img width="150" src="project-management.svg" alt="description ofgggg "/>
+        
+        </section>
+
+        <section style={{
+           backgroundColor: '#D0DB97',
+           paddingTop : 8,
+           paddingBottom :8
+            
+          }}>
+        <img width="150" src="letter.svg" alt="description of rrimage"/>
+        </section>
       </div>
     );
   }
