d7bc34d0d2bcab360db07d38e961bc8a9582b7ae
sanidhya70 <ksanidhya007@gmail.com>
2019-02-22 01:41:44 -0500

    Changes to about page

--- src/components/about.js
+++ src/components/about.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NavLink} from 'react-router-dom';
+import { Grid, Cell} from 'react-mdl';
 
 
 const About=()=>{
@@ -21,48 +22,37 @@ return (
   
   </section>
 
-  <div class="row" style={{
-     backgroundColor: '#B33951'
-    }}>
-  <div class="column"> <img style={{ float: 'right' , paddingRight :8, paddingBottom :8}} width="250" src="me.jpg" alt="description dfffof " className="avatar-img"/></div>
-  <div class="column"> <h1 style={{ color : '#FFFFFF', verticalAlign: 'baseline'}} >Hi, i am Sanidhya Keluskar</h1><h2 style={{ color : '#FFFFFF'}} >Software Engineer & Full Stack Developer</h2></div>
-</div >
-<div className="technologies-known" style={{
-     backgroundColor: '#B33951'
-    }}>
-<img width="40" src="icons8-java.svg" alt="description dfffof "/> 
-<img width="40" src="javascript.svg" alt="description dfffof "/> 
-<img width="40" src="react.svg" alt="description dfffof "/> 
-<img width="40" src="android.svg" alt="description dfffof "/> 
-<img width="40" src="ios.svg" alt="description dfffof "/> 
-<img width="40" src="mysql.svg" alt="description dfffof "/> 
-<img width="40" src="mongo.svg" alt="description dfffof "/> 
-<img width="40" src="nodejs.svg" alt="description dfffof "/> 
-<img width="40" src="spring.svg" alt="description dfffof "/> 
-<img width="40" src="docker.svg" alt="description dfffof "/> 
-<img width="40" src="aws.svg" alt="description dfffof "/> 
-</div>
+ <Grid className="about-grid">
+        <Cell col={6}><img style={{ paddingRight :8, paddingBottom :8}} width="250" src="me.jpg" alt="description dfffof " className="avatar-img"/></Cell>
+        <Cell col={6}><h1 style={{ color : '#FFFFFF', fontSize : 35}} >Hi, i am Sanidhya Keluskar</h1> 
+        <p style={{ color : '#FFFFFF', fontSize : 25}} >Graduate CS student at <img width="80" src="GWU.svg" alt="description dfffof "/> </p>
+        <p style={{ color : '#FFFFFF', fontSize : 25}} >Full Stack Software Developer</p></Cell>
+        
+    </Grid>
 
-<div className = "letter" style={{
-     paddingTop : 8,
-     paddingBottom :8,
+    <Grid className="about-grid">
+        <Cell col={1}><img width="40" src="icons8-java.svg" alt="description dfffof "/></Cell>
+        <Cell col={1}><img width="40" src="javascript.svg" alt="description dfffof "/> </Cell>
+        <Cell col={1}><img width="40" src="react.svg" alt="description dfffof "/> </Cell>
+        <Cell col={1}><img width="40" src="android.svg" alt="description dfffof "/></Cell>
+        <Cell col={1}><img width="40" src="ios.svg" alt="description dfffof "/></Cell>
+        <Cell col={1}><img width="40" src="mysql.svg" alt="description dfffof "/></Cell>
+        <Cell col={1}><img width="40" src="mongo.svg" alt="description dfffof "/></Cell>
+        <Cell col={1}><img width="40" src="nodejs.svg" alt="description dfffof "/></Cell>
+        <Cell col={1}><img width="40" src="spring.svg" alt="description dfffof "/> </Cell>
+        <Cell col={1}><img width="40" src="docker.svg" alt="description dfffof "/> </Cell>
+        <Cell col={1}><img width="40" src="aws.svg" alt="description dfffof "/> </Cell>
+        <Cell col={1}><img width="40" src="git.svg" alt="description dfffof "/> </Cell>
+    </Grid>
 
-    paddingLeft : 8
-      
-    }} >
-
-    <p style={{
-     alignContent: 'center',
-     color : '#FFFFFF'
-    }}> "Self-directed, highly motivated Computer Science graduate with ability to think creatively to solve technology related problems. Strong understanding of algorithms, Data Structures & Object oriented design. Passionate in designing and crafting efficient modern softwares, and learning new technologies and tools if need arises"</p>
-
-    <p style={{
-     alignContent: 'center',
-     color : '#FFFFFF'
-    }}> "Problem solver with strong understanding of algorithms, Data Structures & Object oriented design . Proficient with java, javascript and SQL. Experience with building backend server using Spring boot & Nodejs and deploying on AWS, Heroku. Experience with frontend technologies like Reactjs, HTML,CSS. Comfortable developing mobile app both on IOS & android platform. I am good at troubleshooting and debugging"</p>
+    <Grid className="demo-grid-1">
+        <Cell col={6}>
+        <p style={{color : '#FFFFFF', fontSize : 20}}> Self-directed, highly motivated Computer Science graduate with ability to think creatively to solve technology related problems. Strong understanding of algorithms, Data Structures & Object oriented design. Passionate in designing and crafting efficient modern softwares, and learning new technologies and tools if need arises</p>
+        </Cell>
+        <Cell col={6}><p style={{color : '#FFFFFF', fontSize : 20}}> Problem solver with strong understanding of algorithms, Data Structures & Object oriented design . Proficient with java, javascript and SQL. Experience with building backend server using Spring boot & Nodejs and deploying on AWS, Heroku. Experience with frontend technologies like Reactjs, HTML,CSS. Comfortable developing mobile app both on IOS & android platform. I am good at troubleshooting and debugging</p></Cell>
+    </Grid>
 
 
-</div>
 
     
 
--- src/components/projects.js
+++ src/components/projects.js
@@ -105,6 +105,172 @@ class Projects extends Component{
             </Grid>
 
 
+
+
+
+            <Grid className="demo-grid-1">
+                <Cell col={4}>
+                    <Card  shadow={5} style={{minWidth: '450', height: '600',margin: 'auto', justifyContent: 'center'}}>
+                    <CardTitle >Metro Explorer</CardTitle>
+                    <CardText className="technologies-known"  >
+                    <section>
+                        Developed a beautiful IOS app that assists travelers to explore landmarks & restaurants near Washington DC.<br></br>
+                        Exploiting the Yelp & WMATA API the app enables users to find, get direction, favorite & share with friends a landmark.<br></br>
+                        Implemented a feature which allows user to take picture of a restaurant & the app will show ratings. <br></br>Performed Unit Testing. 
+
+                    </section>
+                    <section>
+                    <img width="40" src="ios.svg" alt="description dfffof "/> 
+                    </section>
+                    
+                    </CardText>
+                    
+                    <CardActions border>
+                    <a href="https://github.com/SanidhyaKeluskar/Spring-Server" rel="noopener noreferrer" target="_blank">
+                    <Button colored style={{alignContent: 'center'} } >GitHub</Button>
+                    </a>
+                        
+                    </CardActions>
+                    </Card>
+                </Cell>
+                <Cell col={4}>
+                    <Card  shadow={5} style={{minWidth: '450', height: '600', margin: 'auto', justifyContent: 'center'}}>
+                    <CardTitle >Petify</CardTitle>
+                    <CardText className="technologies-known"  >
+                    <section>
+                            Created an app that helps users find pets for adoption. User can find pets based on user's location, pet's type, breed, age, etc.<br></br>
+                            Implemented voice search feature for finding pets, just speak about characteristics of pet and the app will show customized result.<br></br>
+                            Used Firebase Database to store information of favorite pets.<br></br> Implemented Dual language support (English & Hindi).
+
+                    </section>
+                    <section>
+                    <img width="40" src="android.svg" alt="description dfffof "/> 
+                    
+                    </section>
+                    
+                    </CardText>
+                    
+                    <CardActions border>
+                    <a href="https://github.com/SanidhyaKeluskar/NodeWatson" rel="noopener noreferrer" target="_blank">
+                    <Button colored style={{alignContent: 'center'} } >GitHub</Button>
+                    </a>
+                        
+                    </CardActions>
+                    </Card>
+                </Cell>
+                <Cell col={4}>
+                    <Card  shadow={5} style={{minWidth: '450', margin: 'auto', justifyContent: 'center'}}>
+                    <CardTitle >College Website</CardTitle>
+                    <CardText className="technologies-known"  >
+                    <section>
+                            Led a team of 3 to develop a website which has features such as Online assignment submission & Buy/sell used product portal.<br></br>
+                            Implemented 'Buddy System' wherein a Senior is assigned to a Freshman as mentor using K-mean clustering algorithm. <br></br>
+                            Maintained backend using MS SQL server for managing user data and exercising user authentication.
+                    </section>
+                    <section>
+                    <img width="40" src="react.svg" alt="description dfffof "/> 
+                    <img width="40" src="nodejs.svg" alt="description dfffof "/> 
+                    <img width="40" src="mysql.svg" alt="description dfffof "/> 
+                    
+                    </section>
+                    
+                    </CardText>
+                    
+                    <CardActions border>
+                    <a href="https://github.com/SanidhyaKeluskar/CollegeWebsite" rel="noopener noreferrer" target="_blank">
+                    <Button colored style={{alignContent: 'center'} } >GitHub</Button>
+                    </a>
+                        
+                    </CardActions>
+                    </Card>
+                </Cell>
+            </Grid>
+
+
+
+
+
+
+            <Grid className="demo-grid-1">
+                <Cell col={4}>
+                    <Card  shadow={5} style={{minWidth: '450', height: '600',margin: 'auto', justifyContent: 'center'}}>
+                    <CardTitle >ChatApp</CardTitle>
+                    <CardText className="technologies-known"  >
+                    <section>
+                            Built a Chat app in which messages are synced across logged-in devices in realtime using Firebase's Realtime Database.<br></br>
+                            Used Firebase Auth to implement user authentication. <br></br>Performed dependency injection using Dagger 2 library.
+
+
+                    </section>
+                    <section>
+                    <img width="40" src="android.svg" alt="description dfffof "/> 
+                    <img width="40" src="icons8-java.svg" alt="description dfffof "/> 
+                    </section>
+                    
+                    </CardText>
+                    
+                    <CardActions border>
+                    <a href="https://github.com/SanidhyaKeluskar/ChatApp" rel="noopener noreferrer" target="_blank">
+                    <Button colored style={{alignContent: 'center'} } >GitHub</Button>
+                    </a>
+                        
+                    </CardActions>
+                    </Card>
+                </Cell>
+                <Cell col={4}>
+                    <Card  shadow={5} style={{minWidth: '450', height: '600', margin: 'auto', justifyContent: 'center'}}>
+                    <CardTitle >WonderQuiz</CardTitle>
+                    <CardText className="technologies-known"  >
+                    <section>
+Designed & developed a quiz app based on seven Natural Wonders of the World. <br></br>
+Implemented Scoreboard for score at the end of Quiz <br></br>Also implemented a Progress Bar for answering each question.
+
+
+                    </section>
+                    <section>
+                    <img width="40" src="android.svg" alt="description dfffof "/> 
+                    <img width="40" src="icons8-java.svg" alt="description dfffof "/> 
+                    
+                    </section>
+                    
+                    </CardText>
+                    
+                    <CardActions border>
+                    <a href="https://github.com/SanidhyaKeluskar/WonderQuiz" rel="noopener noreferrer" target="_blank">
+                    <Button colored style={{alignContent: 'center'} } >GitHub</Button>
+                    </a>
+                        
+                    </CardActions>
+                    </Card>
+                </Cell>
+                <Cell col={4}>
+                    <Card  shadow={5} style={{minWidth: '450', margin: 'auto', justifyContent: 'center'}}>
+                    <CardTitle >WeatherApp</CardTitle>
+                    <CardText className="technologies-known"  >
+                    <section>
+                        Designed & developed a weather app which shows weather data of user's current location using phone's GPS.<br></br>
+                        Used OpenWeatherMap API to fetch weather data. 
+
+                    </section>
+                    <section>
+                    <img width="40" src="android.svg" alt="description dfffof "/> 
+                    <img width="40" src="icons8-java.svg" alt="description dfffof "/> 
+                    
+                    </section>
+                    
+                    </CardText>
+                    
+                    <CardActions border>
+                    <a href="https://github.com/SanidhyaKeluskar/WeatherApp" rel="noopener noreferrer" target="_blank">
+                    <Button colored style={{alignContent: 'center'} } >GitHub</Button>
+                    </a>
+                        
+                    </CardActions>
+                    </Card>
+                </Cell>
+            </Grid>
+
+
             </div>
         )
     }
