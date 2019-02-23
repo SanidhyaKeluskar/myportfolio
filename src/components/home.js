dd250309a30c57e2b766ff1f74c69e89c4f739e2
sanidhya70 <ksanidhya007@gmail.com>
2019-02-23 18:33:06 -0500

    Change to home page

--- src/components/home.js
+++ src/components/home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavLink} from 'react-router-dom';
 import Footer from './footer';
-import { Grid, Cell} from 'react-mdl';
+
 
 
 const Home=()=>{
@@ -10,48 +10,48 @@ return (
         
     
 
-   <section className="home-styling" style={{
-     backgroundColor: '#B33951',
-     paddingTop : 8,
-     paddingBottom :8
-      
-    }} >
-     <NavLink to="/about"> <i class="fas fa-smile-beam"/> </NavLink> 
-  </section>
+   <div className="home-styling" style={{
+          backgroundColor: '#B33951',
+          paddingTop : 8,
+          paddingBottom :8,
+          
+          }} >
 
+      <p><NavLink to="/about"> <i class="fas fa-smile-beam"/> </NavLink> About Me</p>
+   
+  </div>
 
-  <section className="home-styling" style={{
-     backgroundColor: '#2274A5',
-     paddingTop : 8,
-     paddingBottom :8
-      
-    }}>
+  <div className="home-styling" style={{
+            backgroundColor: '#2274A5',
+            paddingTop : 8,
+            paddingBottom :8,
+            }} >
 
-    
-    <NavLink to="/experience"> <i class="fas fa-laptop-code"/></NavLink>
+        <p><NavLink to="/experience"> <i class="fas fa-laptop-code"/> </NavLink>Experience</p>
+     
+  </div>
+
+  <div className="home-styling-three" style={{
+            backgroundColor: '#E9F1F7',
+            paddingTop : 8,
+            paddingBottom :8,
+            }} >
+
+        <p><NavLink to="/projects"> <i class="fas fa-code-branch"/> </NavLink>Projects</p>
+     
+  </div>
+
+  <div className="home-styling-four" style={{
+            backgroundColor: '#D0DB97',
+            paddingTop : 8,
+            paddingBottom :8,
+            }} >
+
+        <p><NavLink to="/contact"> <i class="far fa-envelope"/> </NavLink>Contact</p>
+     
+  </div>
+
   
-  </section>
-
-  <section className="home-styling-three" style={{
-     backgroundColor: '#E9F1F7',
-     paddingTop : 8,
-     paddingBottom :8
-      
-    }}>
-    <NavLink to="/projects"> <i class="fas fa-code-branch"/></NavLink>
-  
-  </section>
-
-  <section className="home-styling-three" style={{
-     backgroundColor: '#D0DB97',
-     paddingTop : 8,
-     paddingBottom :8
-      
-    }}>
-  
-    <NavLink to="/contact"> <i class="far fa-envelope"/></NavLink>
-
-  </section>
   <Footer />
 </div>
 );
