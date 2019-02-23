2ab775da5ef6c6316e3e6b9e3c500b08261b53be
sanidhya70 <ksanidhya007@gmail.com>
2019-02-23 17:32:34 -0500

    minor changes

--- src/components/home.js
+++ src/components/home.js
@@ -1,50 +1,58 @@
 import React from 'react';
 import { NavLink} from 'react-router-dom';
+import Footer from './footer';
+import { Grid, Cell} from 'react-mdl';
+
 
 const Home=()=>{
 return (
   <div className="App">
         
-      
-  <section className="home-styling" style={{
+    
+
+   <section className="home-styling" style={{
      backgroundColor: '#B33951',
      paddingTop : 8,
      paddingBottom :8
       
     }} >
-    <NavLink to="/about"> <img width="150" src="employee.svg" alt="description of "/>  </NavLink>
-         
+     <NavLink to="/about"> <i class="fas fa-smile-beam"/> </NavLink> 
   </section>
 
-  <section style={{
+
+  <section className="home-styling" style={{
      backgroundColor: '#2274A5',
      paddingTop : 8,
      paddingBottom :8
       
     }}>
-    <NavLink to="/experience"> <img width="150" src="portfolio.svg" alt="description dfffof "/> </NavLink>
+
+    
+    <NavLink to="/experience"> <i class="fas fa-laptop-code"/></NavLink>
   
   </section>
 
-  <section style={{
+  <section className="home-styling-three" style={{
      backgroundColor: '#E9F1F7',
      paddingTop : 8,
      paddingBottom :8
       
     }}>
-  <NavLink to="/projects"> <img width="150" src="project-management.svg" alt="description dfffof "/></NavLink>
+    <NavLink to="/projects"> <i class="fas fa-code-branch"/></NavLink>
   
   </section>
 
-  <section style={{
+  <section className="home-styling-three" style={{
      backgroundColor: '#D0DB97',
      paddingTop : 8,
      paddingBottom :8
       
     }}>
-    <NavLink to="/contact"> <img width="150" src="letter.svg" alt="description dfffof "/></NavLink>
+  
+    <NavLink to="/contact"> <i class="far fa-envelope"/></NavLink>
 
   </section>
+  <Footer />
 </div>
 );
 };
